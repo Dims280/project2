1065ebab8f6fe06ce9e771d23bcb51b13a61c819
Dims280 <dimaskrn28@gmail.com>
2025-01-23 20:17:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+import math
+from turtle import*
+
+def hearta(k):
+    return 15*math.sin(k)**3
+def heartb(k):
+    return 12*math.cos(k)-5*\
+    math.cos(2*k)-2*\
+    math.cos(3*k)-\
+    math.cos(4*k)
+speed (0)
+bgcolor("black")
+for i in range(6000):
+    goto(hearta(i)*20, heartb(i)*20)
+    for j in range(5):
+        color("red")
+    goto (0,0)
+done()
 )
